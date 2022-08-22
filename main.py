59b59c8ea8c76577eafe48ff126d4a4c463b9e08
Dolrey <111716809+Dolrey@users.noreply.github.com>
2022-08-22 16:50:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,6 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
-data = {"t_today":{"value":t_today,"color":get_random_color()},"weather":{"value":wea,"color":get_random_color()},"city":{"value":city,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"color":get_random_color(),"value":get_birthday()},"bbirthday_left":{"color":get_random_color(),"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()},"shi":{"value":get_shi(), "color":get_random_color()}}
+data = {"t_today":{"value":t_today,"color":get_random_color()},"weather":{"value":wea,"color":get_random_color()},"city":{"value":city,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"color":get_random_color(),"value":get_birthday()},"bbirthday_left":{"color":get_random_color(),"value":get_bbirthday()},"words":{"value":get_words(), "color":get_random_color()},"shi":{"value":get_shi(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
